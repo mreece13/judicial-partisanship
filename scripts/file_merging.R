dec3c9503bde6984f286d12b226aba0f69261589
Mason Reece <mpreece@mit.edu>
2024-04-01 17:14:07 -0400

    Update file_merging.R

--- scripts/file_merging.R
+++ scripts/file_merging.R
@@ -3,7 +3,7 @@ gc()
 
 library(tidyverse)
 library(arrow)
-# library(data.table)
+library(data.table)
 
 # courts <- read_csv("data/courts-2024-03-11.csv") |>
 #   filter(jurisdiction == "S", in_use, has_opinion_scraper, str_detect(full_name, "Supreme")) |>
@@ -32,11 +32,11 @@ library(arrow)
 #   rename(date_modified_opinion = date_modified) |>
 #   write_dataset("data/merged/", format = "parquet")
 
-f <- function(x, pos) select(x, id, date_modified, type, plain_text, author_id, cluster_id, page_count, author_str)
-read_csv_chunked("data/opinions-2024-03-11.csv",
-                 DataFrameCallback$new(f),
-                 chunk_size = 1000) |> 
-  write_csv("data/opinions-2024-03-11-filtered.csv")
+# f <- function(x, pos) select(x, id, date_modified, type, plain_text, author_id, cluster_id, page_count, author_str)
+# read_csv_chunked("data/opinions-2024-03-11.csv",
+#                  DataFrameCallback$new(f),
+#                  chunk_size = 1000) |> 
+#   write_csv("data/opinions-2024-03-11-filtered.csv")
 
 # read_csv_arrow("data/opinions-2024-03-11.csv", 
 #                read_options = list(block_size = 41943040L),
@@ -44,45 +44,69 @@ read_csv_chunked("data/opinions-2024-03-11.csv",
 #   select(id, date_modified, type, plain_text, author_id, cluster_id, page_count, author_str) |> 
 #   write_csv_arrow("data/opinions-2024-03-11-filtered.csv")
 
-# courts <- fread("data/courts-2024-03-11.csv")[
-#   jurisdiction == "S" &
-#     in_use == "t" &
-#     has_opinion_scraper == "t" &
-#     str_detect(full_name, "Supreme"),
-#   id
-# ]
+courts <- fread("data/courts-2024-03-11.csv")[
+  jurisdiction == "S" &
+    in_use == "t" &
+    has_opinion_scraper == "t" &
+    str_detect(full_name, "Supreme"),
+  id
+]
+
+message("Loading Dockets")
+
+dockets <- fread("data/dockets-2024-03-11.csv",
+  index = c("id", "court_id"),
+  showProgress = TRUE,
+  select = c(
+    "id", "case_name", "docket_number",
+    "court_id", "assigned_to_id", "cause", "jurisdiction_type"
+  )
+)[
+  court_id %in% courts
+]
+
+write_parquet("data/dockets.parquet")
+
+message("Loading Clusters")
+
+clusters <- fread("data/opinion-clusters-2024-03-11.csv",
+  index = c("id", "docket_id"),
+  showProgress = TRUE,
+  select = c(
+    "id", "judges", "case_name", "attorneys",
+    "nature_of_suit", "posture", "syllabus", "citation_count",
+    "precedential_status", "docket_id", "headnotes", "history",
+    "other_dates", "summary"
+  )
+)[
+  dockets,
+  on = c(docket_id = "id"), nomatch = NULL
+]
+
+write_parquet("data/opinion-clusters.parquet")
+
+message("Loading Opinions")
+
+files <- list.files(path = "data/", pattern = "opinions_*", full.names = TRUE)
+
+message(sprintf("Number of Opinion Files: %s", length(files)))
+
+# f <- function(p){
+#   fread(p,
+#         index = "cluster_id",
+#         showProgress = TRUE,
+#         select = c(
+#           "id", "date_modified", "type", "plain_text", "author_id",
+#           "cluster_id", "page_count", "author_str"
+#         )
+#   )[
+#     clusters,
+#     on = c(cluster_id = "id"), nomatch = NULL
+#   ]
+# }
 # 
-# message("Loading Dockets")
-# 
-# dockets <- fread("data/dockets-2024-03-11.csv",
-#   index = c("id", "court_id"),
-#   showProgress = TRUE,
-#   select = c(
-#     "id", "case_name", "docket_number",
-#     "court_id", "assigned_to_id", "cause", "jurisdiction_type"
-#   )
-# )[
-#   court_id %in% courts
-# ]
-# 
-# message("Loading Clusters")
-# 
-# clusters <- fread("data/opinion-clusters-2024-03-11.csv",
-#   index = c("id", "docket_id"),
-#   showProgress = TRUE,
-#   select = c(
-#     "id", "judges", "case_name", "attorneys",
-#     "nature_of_suit", "posture", "syllabus", "citation_count",
-#     "precedential_status", "docket_id", "headnotes", "history",
-#     "other_dates", "summary"
-#   )
-# )[
-#   dockets,
-#   on = c(docket_id = "id"), nomatch = NULL
-# ]
-# 
-# message("Loading Opinions")
-# 
+# dts <- lapply(files, f) |> rbindlist()
+
 # opinions <- fread("data/opinions-2024-03-11.csv",
 #   index = "cluster_id",
 #   showProgress = TRUE,
@@ -94,7 +118,7 @@ read_csv_chunked("data/opinions-2024-03-11.csv",
 #   clusters,
 #   on = c(cluster_id = "id"), nomatch = NULL
 # ]
-# 
+ 
 # message("Writing Opinions")
 # 
-# write_parquet(opinions, "data/merged.parquet")
+# write_parquet(dts, "data/merged.parquet")
