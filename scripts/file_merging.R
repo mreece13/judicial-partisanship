3172cc19f65147b1a7d579a2ad08a3730c5e5c46
Mason Reece <mpreece@mit.edu>
2024-04-01 14:17:53 -0400

    Update file_merging.R

--- scripts/file_merging.R
+++ scripts/file_merging.R
@@ -8,12 +8,12 @@ library(data.table)
 # courts <- read_csv("data/courts-2024-03-11.csv") |>
 #   filter(jurisdiction == "S", in_use, has_opinion_scraper, str_detect(full_name, "Supreme")) |>
 #   select(id)
-# 
+#
 # dockets <- open_csv_dataset("data/dockets-2024-03-11.csv") |>
 #   select(id, date_modified, case_name, docket_number, court_id, assigned_to_id, cause, jurisdiction_type) |>
 #   inner_join(courts, by = c("court_id" = "id")) |>
 #   rename(date_modified_docket = date_modified)
-# 
+#
 # clusters <- open_csv_dataset("data/opinion-clusters-2024-03-11.csv",
 #   parse_options = csv_parse_options(newlines_in_values = TRUE)
 # ) |>
@@ -23,7 +23,7 @@ library(data.table)
 #   ) |>
 #   inner_join(dockets, by = c("docket_id" = "id")) |>
 #   rename(date_modified_clusters = date_modified)
-# 
+#
 # open_csv_dataset("data/opinions-2024-03-11.csv",
 #   parse_options = csv_parse_options(newlines_in_values = TRUE)
 # ) |>
@@ -41,11 +41,13 @@ courts <- fread("data/courts-2024-03-11.csv")[
   .(id)
 ]
 
+message("Loading Dockets")
+
 dockets <- fread("data/dockets-2024-03-11.csv",
-  nrows = 1e6,
-  index = "id,court_id",
+  index = c("id", "court_id"),
+  showProgress = TRUE,
   select = c(
-    "id", "date_modified", "case_name", "docket_number",
+    "id", "case_name", "docket_number",
     "court_id", "assigned_to_id", "cause", "jurisdiction_type"
   )
 )[
@@ -53,10 +55,13 @@ dockets <- fread("data/dockets-2024-03-11.csv",
   on = .(court_id = "id"), nomatch = NULL
 ]
 
-clusters <- fread("data/opinion-clusters-2024-03-11.csv.bz2",
-  index = "id,docket_id",
+message("Loading Clusters")
+
+clusters <- fread("data/opinion-clusters-2024-03-11.csv",
+  index = c("id", "docket_id"),
+  showProgress = TRUE,
   select = c(
-    "id", "judges", "date_modified", "case_name", "attorneys",
+    "id", "judges", "case_name", "attorneys",
     "nature_of_suit", "posture", "syllabus", "citation_count",
     "precedential_status", "docket_id", "headnotes", "history",
     "other_dates", "summary"
@@ -66,8 +71,11 @@ clusters <- fread("data/opinion-clusters-2024-03-11.csv.bz2",
   on = .(docket_id = "id"), nomatch = NULL
 ]
 
+message("Loading Opinions")
+
 opinions <- fread("data/opinions-2024-03-11.csv",
   index = "cluster_id",
+  showProgress = TRUE,
   select = c(
     "id", "date_modified", "type", "plain_text", "author_id",
     "cluster_id", "page_count", "author_str"
@@ -77,5 +85,6 @@ opinions <- fread("data/opinions-2024-03-11.csv",
   on = .(cluster_id = "id"), nomatch = NULL
 ]
 
+message("Writing Opinions")
 
 write_parquet(opinions, "data/merged.parquet")
