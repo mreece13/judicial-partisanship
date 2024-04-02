f14ea628694ac283da99ec447865286e4986b29e
Mason Reece <mpreece@mit.edu>
2024-04-01 20:52:18 -0400

    Update file_merging.R

--- scripts/file_merging.R
+++ scripts/file_merging.R
@@ -5,37 +5,11 @@ library(tidyverse)
 library(arrow)
 library(data.table)
 
-# courts <- read_csv("data/courts-2024-03-11.csv") |>
-#   filter(jurisdiction == "S", in_use, has_opinion_scraper, str_detect(full_name, "Supreme")) |>
-#   select(id)
-#
-# dockets <- open_csv_dataset("data/dockets-2024-03-11.csv") |>
-#   select(id, date_modified, case_name, docket_number, court_id, assigned_to_id, cause, jurisdiction_type) |>
-#   inner_join(courts, by = c("court_id" = "id")) |>
-#   rename(date_modified_docket = date_modified)
-#
-# clusters <- open_csv_dataset("data/opinion-clusters-2024-03-11.csv",
-#   parse_options = csv_parse_options(newlines_in_values = TRUE)
-# ) |>
-#   select(
-#     id, judges, date_modified, case_name, attorneys, nature_of_suit, posture, syllabus,
-#     citation_count, precedential_status, docket_id, headnotes, history, other_dates, summary
-#   ) |>
-#   inner_join(dockets, by = c("docket_id" = "id")) |>
-#   rename(date_modified_clusters = date_modified)
-#
-# open_csv_dataset("data/opinions-2024-03-11.csv",
-#   parse_options = csv_parse_options(newlines_in_values = TRUE)
-# ) |>
-#   select(id, date_modified, type, plain_text, author_id, cluster_id, page_count, author_str) |>
-#   inner_join(clusters, by = c("cluster_id" = "id")) |>
-#   rename(date_modified_opinion = date_modified) |>
-#   write_dataset("data/merged/", format = "parquet")
-
 # f <- function(x, pos) write_csv(x, str_c("data/opinion_chunk_", pos, ".csv"))
-#
+f <- function(x, pos) x
+
 # read_csv_chunked("data/opinions-2024-03-11.csv",
-#                  SideEffectChunkCallback$new(f),
+#                  DataFrameCallback$new(f),
 #                  col_types = cols_only(
 #                    "id" = "i",
 #                    "date_modified" = "T",
@@ -53,9 +27,13 @@ library(data.table)
 #     str_detect(full_name, "Supreme"),
 #   id
 # ]
-#
-# message("Loading Dockets")
-#
+
+message("Loading Dockets")
+
+dockets <- read_parquet("data/dockets.parquet")
+setDT(dockets)
+dockets[id := as.integer(id),]
+
 # dockets <- fread("data/dockets-2024-03-11.csv",
 #   index = c("id", "court_id"),
 #   showProgress = TRUE,
@@ -66,59 +44,41 @@ library(data.table)
 # )[
 #   court_id %in% courts
 # ]
-#
+# 
 # write_parquet(dockets, "data/dockets.parquet")
 
 message("Loading Clusters")
 
-clusters <- read_parquet("data/opinion-clusters.parquet")
-setDT(clusters)
+# clusters <- read_parquet("data/opinion-clusters.parquet")
+# setDT(clusters)
 
-# clusters <- fread("data/opinion-clusters-2024-03-11.csv",
-#   index = c("id", "docket_id"),
-#   showProgress = TRUE,
-#   select = c(
-#     "id", "judges", "case_name", "attorneys",
-#     "nature_of_suit", "posture", "syllabus", "citation_count",
-#     "precedential_status", "docket_id", "headnotes", "history",
-#     "other_dates", "summary"
-#   )
-# )[
-#   dockets,
-#   on = c(docket_id = "id"), nomatch = NULL
-# ]
-#
-# write_parquet(clusters, "data/opinion-clusters.parquet")
+clusters <- fread("data/opinion-clusters-2024-03-11.csv",
+  index = c("id", "docket_id"),
+  showProgress = TRUE,
+  select = c(
+    "id", "judges", "case_name", "attorneys", "date_filed",
+    "nature_of_suit", "posture", "syllabus", "citation_count",
+    "precedential_status", "docket_id", "headnotes", "history",
+    "other_dates", "summary"
+  )
+)[
+  dockets,
+  on = c(docket_id = "id"), nomatch = NULL
+]
+
+clusters[id := as.integer(id), ]
+
+write_parquet(clusters, "data/opinion-clusters.parquet")
 
 message("Loading Opinions")
 
-# files <- list.files(path = "data", pattern = stringr::fixed("opinions_"), full.names = TRUE)
-#
-# message(sprintf("Number of Opinion Files: %s", length(files)))
-#
-# f <- function(p){
-#   message(p)
-#
-#   fread(file = p,
-#         sep = ",",
-#         index = "cluster_id",
-#         # showProgress = TRUE,
-#         select = c(
-#           "id", "date_modified", "type", "plain_text", "author_id",
-#           "cluster_id", "page_count", "author_str"
-#         )
-#   )[
-#     clusters,
-#     on = c(cluster_id = "id"), nomatch = NULL
-#   ]
-# }
-#
-
 opinions <- fread(
-  cmd = "tail -n-1 -q data/opinion_chunk*.csv",
+  path = "data/opinion_chunk_1.csv",
+  # cmd = "tail -n-1 -q data/opinion_chunk*.csv",
   index = "cluster_id",
   showProgress = TRUE,
-  col.names = c("id", "date_modified", "type", "plain_text", "author_id", "cluster_id")
+  fill = TRUE,
+  col.names = c("id", "type", "plain_text", "author_id", "cluster_id")
 )[
   clusters,
   on = c(cluster_id = "id"), nomatch = NULL
